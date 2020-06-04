bfbb762250f32f519b5c1941be4876a582da554a
Tarcisio Franco <tarcisiofcarvalho@yahoo.com.br>
2020-06-04 08:04:34 -0300

    kinect added

--- sim_model/scripts/launch.sh
+++ sim_model/scripts/launch.sh
@@ -11,5 +11,6 @@ clear
 # roslaunch robot_cognition_system display.launch model:=../urdf/06-flexible.urdf
 # roslaunch robot_cognition_system display.launch model:=../urdf/07-physics.urdf
 # roslaunch robot_cognition_system display.launch model:=../urdf/08-macroed.urdf.xacro
-# roslaunch robot_cognition_system display.launch model:=../urdf/sim_model.urdf.xacro
-roslaunch robot_cognition_system gazebo.launch
+# roslaunch robot_cognition_system gazebo.launch
+roslaunch robot_cognition_system display.launch model:=../urdf/sim_model.urdf.xacro
+# roslaunch robot_cognition_system diffdrive.launch
