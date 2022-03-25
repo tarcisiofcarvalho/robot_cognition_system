4ca42aa68b8d3de401ad9ab53d617e43e9bc0d10
Tarcisio Franco <tarcisiofcarvalho@yahoo.com.br>
2022-03-25 19:20:50 -0300

    cont.. arduino serial

--- arduino/wheelchair-cognition/test_arduino.py
+++ arduino/wheelchair-cognition/test_arduino.py
@@ -1,7 +1,15 @@
 import serial
 
-ser = serial.Serial("/dev/ttyACM0",9600)
+ser = serial.Serial("/dev/ttyACM1",9600)
 
 while 1:
-    var=raw_input("Enter 1 or 0")
+    var=raw_input("Enter 0 or 1: ")
     ser.write(var)
+
+# pan = 0 - 80 - 160
+# tilt = 1 - 80
+
+
+# define the math conversion from deg/rad to servo motor 0>255
+# code arduino to receive <servo>:<deg/rad> and apply in servo
+# code ROS Node to receive <servo>:<deg/rad> and send to arduino
--- arduino/wheelchair-cognition/wheelchair-cognition.ino
+++ arduino/wheelchair-cognition/wheelchair-cognition.ino
@@ -1,58 +1,88 @@
 #include <Servo.h>
 
-int servoMPin = 9;
-int servoLPin = 6;
-//int servoRPin = 5;
+int servo_pan_pin = 9;
+int servo_tilt_pin = 6;
 
-int mid_servo_right = 50;
-int mid_servo_base = 100;
-int mid_servo_left = 200;
+int servo_pan_start = (80);
+int servo_tilt_start = (100);
 
-//int front_right_servo_forward = 130;
-//int front_right_servo_base = 100;
+int servo_pan_current = servo_pan_start;
+int servo_tilt_current = servo_tilt_start;
 
-int front_left_servo_forward = 100;
-int front_left_servo_base = 200;
-Servo servoM;
-Servo servoL;
-//Servo servoR;
+bool green_target_led = false;
+
+Servo servoPan;
+Servo servoTilt;
+
 
 void setup() {   
+
+  // 1. Serial interface setup 
   Serial.begin(9600); // sets the boud rate to 9600 bts
   pinMode(13,OUTPUT); // configures the onboard led as output
+  pinMode(4,OUTPUT); // Laser led pin (red)
+  pinMode(2,OUTPUT); // Led (green)
   
-  servoM.attach(servoMPin);
-  servoM.write(mid_servo_base);
+  // 2. Pan servo setup
+  servoPan.attach(servo_pan_pin);
+  servoPan.write(servo_pan_start);
   delay(200); 
-  servoL.attach(servoLPin);
-  servoL.write(front_left_servo_base);
-  delay(200);  
-//  servoR.attach(servoRPin);
-//  servoR.write(front_right_servo_base);
-//  delay(200); 
+
+  // 3. Tilt servo setup
+  servoTilt.attach(servo_tilt_pin);
+  servoTilt.write(servo_tilt_start);
+  delay(200); 
+ 
 }
 
 void loop() {
 
   bool x = true;
   while(x){
-
-    // String s1 = "Hello World";
-    // s1.replace('e','a');
-    // myString.startsWith(myString2)
-    // myString.toInt()
     
     if(Serial.available()){
-      switch(Serial.read()){
-        case '0': digitalWrite(13,LOW);
-                  break;
-        case '1': digitalWrite(13,HIGH);
-                  break;
-        default:
-                  break;
+      
+      // ----------------------------------------- //
+      // ----- Incoming command validation ------- //
+      // ----------------------------------------- //
+      String serial_data;
+      serial_data = Serial.readString();
+      if(serial_data.startsWith("pan_")){
+        digitalWrite(13,HIGH);
+        serial_data.replace("pan_","");
+        servo_pan_current = serial_data.toInt();
+        green_target_led = false;
+      }else if(serial_data.startsWith("tilt_")){
+        digitalWrite(13,HIGH);
+        serial_data.replace("tilt_","");
+        servo_tilt_current = serial_data.toInt();
+        green_target_led = false;
+      }else if(serial_data.startsWith("led_")){
+        serial_data.replace("led_","");
+        if(serial_data == "on"){
+          green_target_led = true;
+        }else{
+          green_target_led = false;
+        }
       }
+
+      // ----------------------------------------- //
+      // ------------ Command action ------------- //
+      // ----------------------------------------- //      
+      servoPan.write(servo_pan_current);
+      servoTilt.write(servo_tilt_current);
+      delay(100);
       
     }
+    
+    if(green_target_led){
+      digitalWrite(2,HIGH);
+    }else{
+      digitalWrite(2,LOW);
+    }
+    // Laser Led Always Active
+    digitalWrite(4,HIGH);
+    
 //      // 1. Up middle right base
 //      servoM.write(mid_servo_right);  
 //      delay(500); 
